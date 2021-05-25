09d44496d98c4d4f5da134c9491f2ad0717d1f0c
tolumide-ng <tolumideshopein@gmail.com>
2021-05-25 23:29:58 +0100

    replaces res on src/index with responseGenerator helper

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { ResponseGenerator } from "./helpers/responseGenerator/index.helper";
 
 dotenv.config();
 const app = express();
@@ -8,7 +9,7 @@ app.use(express.json({}));
 app.use(cors());
 
 app.use("*", (req, res) => {
-    res.status(200).send({ message: "welcome to email_server :eyes" });
+    ResponseGenerator.sendError(res, 404);
 });
 
 const PORT = process.env.PORT || 4000;
