58b9a4338a5cbd791f8878f6bf0ace1135e854d7
tolumide-ng <tolumideshopein@gmail.com>
2021-05-26 14:57:25 +0100

    adds more validator functions

--- src/validators/mail/index.validator.ts
+++ src/validators/mail/index.validator.ts
@@ -16,3 +16,30 @@ export const hasToken: RequestHandler = async (req, res, next) => {
         return ResponseGenerator.sendError(res, 401);
     } catch (error) {}
 };
+
+export const hasLogin: RequestHandler = async (req, res, next) => {
+    if (!req.body.password || !req.body.email) {
+        return ResponseGenerator.sendError(res, 400);
+    }
+
+    next();
+};
+
+export const hasValidEmail: RequestHandler = async (req, res, next) => {
+    if (req.body.email) {
+        const isEmailValid =
+            /^\w+([\.-]?\w+)+@\w+([\.:]?\w+)+(\.[a-zA-Z0-9]{2,})+$/.test(
+                req.body.email,
+            );
+
+        if (isEmailValid) {
+            return next();
+        }
+    }
+
+    return ResponseGenerator.sendError(
+        res,
+        400,
+        "Please provide a valid email address",
+    );
+};
