ca3b442a627c9d5dc3e32500877e8b16b201abce
tolumide-ng <tolumideshopein@gmail.com>
2021-05-28 12:11:27 +0100

    creates controller for get specific inbox message

--- src/controllers/mails/index.controller.ts
+++ src/controllers/mails/index.controller.ts
@@ -6,14 +6,45 @@ import { getPop3Inbox } from "../../helpers/mailFunctions/pop3";
 import { ResponseGenerator } from "../../helpers/responseGenerator/index.helper";
 
 export class MailController {
+    static hostDict: { [key: string]: string } = {
+        POP3: "pop.mail.yahoo.com",
+        IMAP: "imap.mail.yahoo.com",
+    };
+
     static async getAllInbox(req: Request, res: Response) {
         try {
             const { email, password, serverType, encType } = req.body;
 
-            const host =
-                serverType === "POP3"
-                    ? "pop.mail.yahoo.com"
-                    : "imap.mail.yahoo.com";
+            const host = MailController.hostDict[serverType];
+
+            const getUserInbox = new MailFunction({
+                email,
+                password,
+                serverType,
+                encryption: encType,
+                host,
+                action: "all",
+            });
+
+            if (serverType === "IMAP") {
+                getUserInbox.getInbox(getImapInbox, res);
+            }
+
+            if (serverType === "POP3") {
+                getUserInbox.getInbox(getPop3Inbox, res);
+            }
+        } catch (error) {
+            return ResponseGenerator.sendError(res, 500);
+        }
+    }
+
+    static async getSpecificInbox(req: Request, res: Response) {
+        try {
+            const { email, password, serverType, encType } = req.body;
+
+            const { id } = req.params;
+
+            const host = MailController.hostDict[serverType];
 
             const getUserInbox = new MailFunction({
                 email,
@@ -22,7 +53,7 @@ export class MailController {
                 encryption: encType,
                 host,
                 action: "one",
-                msgNumber: 1,
+                msgNumber: Number(id),
             });
 
             if (serverType === "IMAP") {
