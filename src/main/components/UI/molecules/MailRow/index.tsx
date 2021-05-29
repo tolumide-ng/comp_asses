0413ebf044d28bc62ba1b4e8061ca55d60f0f2ab
tolumide-ng <tolumideshopein@gmail.com>
2021-05-29 23:10:08 +0100

    displays email date on mail row

--- src/main/components/UI/molecules/MailRow/index.tsx
+++ src/main/components/UI/molecules/MailRow/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { MailSenderDef } from "../../../../declarations";
 import { Initials } from "../../atoms/Initials";
 import style from "./index.module.css";
+import dayjs from "dayjs";
 
 interface MailRowDef {
     subject: string;
@@ -17,11 +18,18 @@ export const MailRow = (props: MailRowDef) => {
                 <Initials name={props?.from?.name} />
             </div>
             <div className={style.mailRowRight}>
-                <div className={style.mailRowRightTop}>
-                    <p className={style.mailRowName}>{props.from.name}</p>
+                <div className={style.mailRowSum}>
+                    <div className={style.mailRowRightTop}>
+                        <p className={style.mailRowName}>{props.from.name}</p>
+                    </div>
+                    <div className={style.mailRowRightBottom}>
+                        <p className={style.mailRowSubject}>{props.subject}</p>
+                    </div>
                 </div>
-                <div className={style.mailRowRightBottom}>
-                    <p className={style.mailRowSubject}>{props.subject}</p>
+                <div className={style.mailRowDateCont}>
+                    <p className={style.mailRowDate}>
+                        {dayjs(props.date).format("MMM D YYYY")}
+                    </p>
                 </div>
             </div>
         </div>
