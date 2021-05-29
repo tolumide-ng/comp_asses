246ee186be1a65f4b3d3d569104d011f00bf1126
tolumide-ng <tolumideshopein@gmail.com>
2021-05-29 11:10:45 +0100

    handles mobile responsiveness

--- src/main/components/Pages/LandingPage/index.tsx
+++ src/main/components/Pages/LandingPage/index.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
+import { Connection } from "../../UI/atoms/organisms/Connection";
 import style from "./index.module.css";
 
 export const LandingPage = () => {
+    const [displaySpecific, setDisplaySpecific] = React.useState(false);
+
     return (
         <article className={style.homeCont}>
             <div className={style.home}>
                 <div className={`${style.homeLeft} ${style.homeChild}`}>
-                    <div className={style.homeleftTop}></div>
+                    <div className={style.homeleftTop}>
+                        <Connection />
+                    </div>
                     <div className={style.homeLeftBottom}></div>
                 </div>
                 <div className={`${style.homeRight} ${style.homeChild}`}></div>
--- src/main/components/UI/atoms/organisms/Connection/index.tsx
+++ src/main/components/UI/atoms/organisms/Connection/index.tsx
@@ -2,5 +2,7 @@ import * as React from "react";
 import style from "./index.module.css";
 
 export const Connection = () => {
-    return <section></section>;
+    return <section>
+        
+    </section>;
 };
