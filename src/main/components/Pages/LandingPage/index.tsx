a1615584eeb986595dc2cac1c72ae92ad016f4f6
tolumide-ng <tolumideshopein@gmail.com>
2021-05-29 10:44:15 +0100

    styles application layout

--- src/main/components/Pages/LandingPage/index.tsx
+++ src/main/components/Pages/LandingPage/index.tsx
@@ -3,72 +3,14 @@ import style from "./index.module.css";
 
 export const LandingPage = () => {
     return (
-        <article className="ldpg">
-            <article className="ldpg-cont"></article>
-
-            <table
-                className={style.frontPageSettingsTable}
-                width="100%"
-                // height="100%"
-            >
-                <tr>
-                    <td valign="top">
-                        <table
-                            className={style.frontPageSettingsTableInner}
-                            width="100%"
-                        >
-                            <tr>
-                                <td>Server type</td>
-                                <td align="right">
-                                    <select />
-                                </td>
-                                <td>Encryption</td>
-                                <td align="right">
-                                    <select></select>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Server</td>
-                                <td align="right">
-                                    <input type="text" value="imap.gmx.com" />
-                                </td>
-                                <td>Username</td>
-                                <td align="right">
-                                    <input type="text" value="" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Port</td>
-                                <td align="right">
-                                    <input type="text" value="993" />
-                                </td>
-                                <td>Password</td>
-                                <td align="right">
-                                    <input type="text" value="" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colSpan={5} align="right">
-                                    <input type="button" value="Start" />
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                    <td
-                        rowSpan={2}
-                        width="600px"
-                        height="800 px"
-                        className={style.frontMessageBody}
-                    >
-                        Body should go here
-                    </td>
-                </tr>
-                <tr>
-                    <td className={style.frontMessageListTd}>
-                        Messages should go here
-                    </td>
-                </tr>
-            </table>
+        <article className={style.homeCont}>
+            <div className={style.home}>
+                <div className={`${style.homeLeft} ${style.homeChild}`}>
+                    <div className={style.homeleftTop}></div>
+                    <div className={style.homeLeftBottom}></div>
+                </div>
+                <div className={`${style.homeRight} ${style.homeChild}`}></div>
+            </div>
         </article>
     );
 };
---
+++ src/main/components/UI/atoms/organisms/Connection/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import style from "./index.module.css";
+
+export const Connection = () => {
+    return <section></section>;
+};
