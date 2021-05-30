3664521acda556bea5d22e95ac5d9604a8104e94
tolumide-ng <tolumideshopein@gmail.com>
2021-05-30 23:02:33 +0100

    refactors mail controller to remove/reduce code duplication

--- src/controllers/mails/index.controller.ts
+++ src/controllers/mails/index.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { FetchActionTypeDef, MailFuncDef } from "../../helpers/mailFunctions";
 
 import { getImapInbox } from "../../helpers/mailFunctions/imap/index.imap";
 import { MailFunction } from "../../helpers/mailFunctions/index.mail";
@@ -11,20 +12,31 @@ export class MailController {
         IMAP: "imap.mail.yahoo.com",
     };
 
-    static async getAllInbox(req: Request, res: Response) {
+    private static async handleRequest(
+        req: Request,
+        res: Response,
+        requestType: FetchActionTypeDef,
+    ) {
         try {
             const { email, password, serverType, encType } = req.body;
 
             const host = MailController.hostDict[serverType];
 
-            const getUserInbox = new MailFunction({
+            const config: MailFuncDef = {
                 email,
                 password,
                 serverType,
                 encryption: encType,
                 host,
-                action: "all",
-            });
+                action: requestType,
+            };
+
+            if (requestType === "one") {
+                const { id } = req.params;
+                config["msgNumber"] = Number(id);
+            }
+
+            const getUserInbox = new MailFunction(config);
 
             if (serverType === "IMAP") {
                 getUserInbox.getInbox(getImapInbox, res);
@@ -38,31 +50,17 @@ export class MailController {
         }
     }
 
+    static async getAllInbox(req: Request, res: Response) {
+        try {
+            await MailController.handleRequest(req, res, "all");
+        } catch (error) {
+            return ResponseGenerator.sendError(res, 500);
+        }
+    }
+
     static async getSpecificInbox(req: Request, res: Response) {
         try {
-            const { email, password, serverType, encType } = req.body;
-
-            const { id } = req.params;
-
-            const host = MailController.hostDict[serverType];
-
-            const getUserInbox = new MailFunction({
-                email,
-                password,
-                serverType,
-                encryption: encType,
-                host,
-                action: "one",
-                msgNumber: Number(id),
-            });
-
-            if (serverType === "IMAP") {
-                getUserInbox.getInbox(getImapInbox, res);
-            }
-
-            if (serverType === "POP3") {
-                await getUserInbox.getInbox(getPop3Inbox, res);
-            }
+            await MailController.handleRequest(req, res, "one");
         } catch (error) {
             return ResponseGenerator.sendError(res, 500);
         }
